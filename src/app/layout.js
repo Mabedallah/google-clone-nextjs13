7cce3c1b1562146d941fbc1f12d6596fadd30f0b
Mabedallah <Mabedallah@users.noreply.github.com>
2023-06-03 23:42:38 +0300

    test tailwindCSS and get the svg icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,13 @@ export const metadata = {
   title: 'Google Clone Next js 13',
   description: 'Google clone created by nextjs 13',
   content: 'width=device-width, initial-scale=1',
+  icons: {
+    icon:'/favicon.svg',},
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en">      
       <body className={inter.className}>{children}</body>
     </html>
   )
