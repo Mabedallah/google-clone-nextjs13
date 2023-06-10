dff5c5093b40d2263095398bd8aa19ce958f3e61
Mabedallah <Mabedallah@users.noreply.github.com>
2023-06-10 22:48:57 +0300

    add footer component

--- src/components/CountryLookup.js
+++ src/components/CountryLookup.js
@@ -1,6 +1,16 @@
-"use clien"
+"use client"
+
+import { useEffect, useState } from "react";
+
 export default function CountryLookup() {
+  const [country, setCountry] = useState("US");
+  console.log(process.env.NEXT_PUBLIC_IP_ADDRESS);
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_IP_ADDRESS}`)
+    .then((res)=>res.json())
+    .then((data)=>setCountry(data.country_name));
+  }, []);  
   return (
-    <div>CountryLookup</div>
+    <div>{country}</div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,18 @@ import CountryLookup from "./CountryLookup";
 
 export default function Footer() {
   return (
-    <footer>
-        <div className="flex flex-col items-center justify-center w-full h-24">
+    <footer className="absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full">
+        <div className="border-b px-8 py-3">
             <CountryLookup/>            
         </div>
-        <div className="flex flex-col items-center justify-center w-full h-24">
-            <ul className="flex flex-row items-center justify-center w-full h-24">
+        <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
+            <ul className="flex items-center space-x-6">
                 <li className="link flex flex-row items-center justify-center w-full h-24">About</li>
                 <li className=" link flex flex-row items-center justify-center w-full h-24">Advertising</li>
                 <li className="link flex flex-row items-center justify-center w-full h-24">Business</li>
                 <li className="link flex flex-row items-center justify-center w-full h-24">How Search works</li>
             </ul>
-            <ul className="flex flex-row items-center justify-center w-full h-24">
+            <ul className="flex items-center space-x-6">
                 <li className="flex link flex-row items-center justify-center w-full h-24">Privacy</li>
                 <li className=" link flex flex-row items-center justify-center w-full h-24">Terms</li>
                 <li className="link flex flex-row items-center justify-center w-full h-24">Settings</li>
